0f357cb7b4c353a8677fb264d1d4ec79661fc598
WithNoRegret <no.regret.ccg@gmail.com>
2024-08-20 11:16:16 +0500

    auth module

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import db from "@astrojs/db";
+import auth from "auth-astro";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), db(), auth()],
+  security: {
+    checkOrigin: true
+  },
+  output: "server",
+  adapter: netlify()
 });
