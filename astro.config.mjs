0b9cf26176d67574b3c56220cbb62b222b23b68e
WithNoRegret <no.regret.ccg@gmail.com>
2024-08-20 11:42:05 +0500

    removing auth module

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import auth from "auth-astro";
-
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), auth()],
+  integrations: [tailwind()],
   security: {
     checkOrigin: true,
   },
